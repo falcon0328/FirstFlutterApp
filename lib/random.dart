417a8513fbc6a5515956defbdbe05e2574e85031
aseo <aseo@yahoo-corp.jp>
2019-05-18 21:17:24 +0900

    feat: 画面遷移機能を追加

--- lib/random.dart
+++ lib/random.dart
@@ -65,7 +65,35 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Startup name Generator")),
+        appBar: AppBar(
+          title: Text("Startup name Generator"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved,
+            )
+          ],
+        ),
         body: _buildSuggestions());
+  }
+
+  /**
+   * リストアイコンを押した時に次の画面に遷移する
+   */
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+      });
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Saved Suggestions"),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 }
