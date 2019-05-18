f6d56e913f2e8decf1ce583e2669ac3a7db9252a
aseo <aseo@yahoo-corp.jp>
2019-05-18 16:47:10 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:english_words/english_words.dart';
+import 'package:hello/random.dart'; // add this line by seo
+
+void main() {
+  // Enable integration testing with the Flutter Driver extension.
+  // See https://flutter.dev/testing/ for more info.
+  enableFlutterDriverExtension();
+  runApp(MyHomePage());
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(),
+    );
+  }
+}
